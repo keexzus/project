bae9c213562d19e9bc4164a3b9c8f5bc9c554f6d
Enrique Avalos <keex@Enriques-MacBook-Air.local>
2023-01-31 12:08:28 -0800

    changed About

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,40 +1,35 @@
 import React from "react";
 import '../index.css';
-// import { Link } from 'react-router-dom'
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Greeting from './greeting.js';
+import {useState} from 'react';
 
+const CounterStateCounter = () => {
+  const [value, setValue] = useState(0);
+  const reset = () => {
+           setValue(0);
+          };
 
+          return(
+      <>
+      <section className='section'>
 
-const About = () => {
-  return (
-    <section >
-      
+        <h2>Skills</h2>
+        <h1>{value}</h1>
+        
+        <button className='btn' onClick={() => setValue
+        (value +1)}>Increase Value 
+        </button>
 
-        <div className='container-2'> 
-      <p>This is a ReactJS site</p>
-         <p>Made by Keex in the USA</p>
-         <p>Goal is to escape the matrix</p>
-         <br></br>
-         <p>Skills:</p>
-         <br></br>
-         <ul>
-          <li>ReactJS</li>
-          <li>JSX</li>
-          <li>CSS</li>
-          <li>React Router</li>
-         <li>Axios HTTP Library</li>
-         <li>Rest API's</li>
-         <li>not an expert in everything,<br></br>
-            but I'll figure it out
-         </li>
-         <li>backend functionality: Summer 2023</li>
-         </ul>
-      </div>
-      
+        <button className='btn' onClick={reset}>
+          reset
+        </button>
+
+      </section>
+
+      </>
     
-    </section>
-  );
-};
+         )
+        };
+    
 
-export default About;
+
+  export default CounterStateCounter;
