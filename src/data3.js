b360c4b6a423ff0712be98e7aa303444fa7ded98
Enrique Avalos <keex@Enriques-Air.lan>
2023-01-31 15:45:27 -0800

    added Test Page

---
+++ src/data3.js
@@ -0,0 +1,14 @@
+
+
+export const data = 
+    
+    [
+
+    { id: 1, skill: 'ReactJS' },
+    { id: 2, skill: 'Reacct Router 6' },
+    { id: 3, skill: 'Axios HTTP Library'},
+    { id: 4, skill: 'JSX'},
+
+];
+
+ export default data
---
+++ src/pages/TestPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import '../index.css';
+import { useState } from 'react'
+
+const ExperimentPage = () => {
+    const [value, setValue] = useState(0);
+    const reset = () => {
+             setValue(0);
+            };
+  
+
+    return(
+        <>
+       
+       <section className='section'>  
+       <h2>Experiment with Code</h2>
+       </section>
+
+        </>
+    );
+};
+
+export default ExperimentPage
