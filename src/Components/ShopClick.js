296440c37eeee85827a34d78e00d16aebcb4ed47
Enrique Avalos <keex@Enriques-Air.lan>
2023-08-02 20:34:23 -0700

    update

--- src/Components/ShopClick.js
+++ src/Components/ShopClick.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-// import { useGlobalContext } from '../context.js'
+
 
 const ShopClick = () => {
 
