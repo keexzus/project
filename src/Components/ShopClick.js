12a472a72ed7c985f4f89da3015838fbd4b368bc
Enrique Avalos <keex@Enriques-Air.lan>
2023-08-02 20:47:16 -0700

    okay

--- src/Components/ShopClick.js
+++ src/Components/ShopClick.js
@@ -6,7 +6,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 const ShopClick = () => {
 
-    // const { openSidebar, openModal } = useGlobalContext()
+    
     return(
         <div> 
 
